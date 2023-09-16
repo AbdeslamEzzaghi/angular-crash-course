cfc4e18ea2ad68a97bcf0af408e464064749e6a3
Abdeslam Ezzaghi <60900845+AbdeslamEzzaghi@users.noreply.github.com>
2023-09-16 17:00:04 +0100

    construtor injection and creating my first service

---
+++ src/app/services/my-first.service.ts
@@ -0,0 +1,47 @@
+import { Injectable } from '@angular/core';
+
+type message = {
+  name: string;
+  email: string;
+  message: string;
+};
+
+@Injectable({
+  providedIn: 'root',
+})
+export class MyFirstService {
+ 
+  messages: Array<any> = [];
+
+  constructor() {
+    this.init();
+  }
+
+  init():void{
+    this.insert({
+      name : 'abde',
+      email : 'ezzalzouli@11.es',
+      message: "loco loco"
+    })
+    this.insert({
+      name : 'riad',
+      email : 'chadi@11.es',
+      message: "barcelona"
+    })
+    this.insert({
+      name : 'ziyech',
+      email : 'ajax@11.es',
+      message: "khoot khoot"
+    })
+  }
+  insert(message: message):void {
+    this.messages.push(message);
+  }
+
+  getAllMessages():any[]{
+      return this.messages;
+  }
+  deleteMessage(index : number):void{
+      this.messages.splice(index,1);
+  }
+}
